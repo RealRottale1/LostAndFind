d544d22746c88b95e160330ad11bda1bca112b7a
RealRottale1 <camarkham2@gmail.com>
2025-01-19 16:12:55 -0500

    "simple" local data storage system
    Added "simple" local data storage system

--- App.js
+++ App.js
@@ -14,6 +14,81 @@ export default function App() {
     {key:'23', value:'Durham County'},
   ];
 
+  // Local User Data Manager
+  class localDataManager {
+    static dataLoaded = false;
+    static loadingFailed = false;
+    static userData = {
+      name: "",
+      tBNP: "0", // Time Before Next Post
+    };
+
+    static async saveUserData() {
+      if (!this.dataLoaded) {
+        console.warn("User data has not been loaded. Unable to save!");
+        return;
+      }
+      if (this.loadingFailed) {
+        console.warn("User data failed to load properly. To avoid corruption, user data will not save!");
+        return;
+      }
+      try {
+        for (let key in this.userData) {
+          await AsyncStorage.setItem(toString(key), this.userData[key]);
+        }
+      } catch (err) {
+        console.warn("Unable to save user data! Error:" + err);
+      }
+    }
+    
+    static async loadUserData() {
+      if (this.dataLoaded) {
+        console.warn("User data has already been loaded. Unable to load!");
+        return;
+      }
+      try {
+        for (let key in this.userData) {
+          const valueData = await AsyncStorage.getItem(toString(key));
+          if (valueData !== null) {
+            this.userData[key] = valueData;
+            console.log(this.userData[key]);
+          }
+        }
+        console.log("User data loaded successfully!");
+        console.log(this.userData);
+      } catch(err) {
+        this.loadingFailed = true;
+        console.warn("Unable to load user data! Error:" + err);
+      } finally {
+        this.dataLoaded = true;
+      }
+    }
+  };
+  localDataManager.loadUserData();
+
+  //HTML
+  return(
+   <SafeAreaView styele={styles.container}>
+    <SelectList
+      searchPlaceholder="Can't find your county? Search for it here!"
+      notFoundText='Sorry but that county does not exist in North Carolina!'
+      data={countiesData}
+      setSelected={setSelected}
+      save='value'
+      onSelect={() => {
+        Alert.alert("Notice", `You selected ${selected}`)
+      }}
+    />
+    <TouchableHighlight onPress={() => {
+      Alert.alert("Notice", "Your lost item data has been sent!")
+      }}>
+      <Text>Submit Data</Text>
+    </TouchableHighlight>
+   </SafeAreaView>
+  )
+}
+
+/*
   const userData = {}
   async function saveUserData() {
     if (userData && userData[0]) {
@@ -32,7 +107,7 @@ export default function App() {
         userData[0] = JSON.parse(stringData)
         console.log(userData[0])
       } else {
-        userData[0] = {name: "", timeBeforePost: 0}
+        userData[0] = {name: "", county: "", timeBeforePost: 0}
       }
       console.log("Done!")
     } catch (err) {
@@ -40,27 +115,7 @@ export default function App() {
     }
   }
   loadUserData()
-
-  //HTML
-  return(
-   <SafeAreaView styele={styles.container}>
-    <SelectList
-      searchPlaceholder="Can't find your county? Search for it here!"
-      notFoundText='Sorry but that county does not exist in North Carolina!'
-      data={countiesData}
-      setSelected={setSelected}
-      save='value'
-      onSelect={() => {Alert.alert("Notice", `You selected ${selected}`)}}
-    />
-    <TouchableHighlight onPress={() => {
-      saveUserData();
-      Alert.alert("Notice", "Your lost item data has been sent!")
-      }}>
-      <Text>Submit Data</Text>
-    </TouchableHighlight>
-   </SafeAreaView>
-  )
-}
+*/
 
 // CSS
 const styles = StyleSheet.create({
